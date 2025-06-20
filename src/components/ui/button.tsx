bc81611f1c950c09fb1ced26b5c7ec9dd8bd9610
Sayanta Nandi <sayantanandi123@gmail.com>
2025-06-20 17:39:23 +0530

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -62,7 +62,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       >
         {isLoading ? (
-          <span className="ml-1.5 flex items-center gap-1">
+          <span className="mx-2 flex items-center gap-1">
             <span className="animate-flashing w-1 h-1 bg-white rounded-full inline-block" />
             <span className="animate-flashing delay-100 w-1 h-1 bg-white rounded-full inline-block" />
             <span className="animate-flashing delay-200 w-1 h-1 bg-white rounded-full inline-block" />
