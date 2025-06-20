eddb7ae7407b626e1e5e977e0f06560dd0340e36
sayanta-nandi <sayantanandi123@gmail.com>
2025-06-20 21:08:07 +0530

    add payment gateway

---
+++ src/app/auth-callback/action.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { prisma } from "@/utils/client";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export async function handleUser() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user?.id || !user?.email) throw new Error("invalid user data");
+
+  const existingUser = await prisma.user.findFirst({
+    where: { id: user.id },
+  });
+
+  if (!existingUser) {
+    await prisma.user.create({
+      data: {
+        email: user.email,
+        id: user.id,
+      },
+    });
+  }
+
+  return { success: true };
+}
