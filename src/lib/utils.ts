b97fecca18a396f02df06d404bad6949244d7d87
sayanta-nandi <sayantanandi123@gmail.com>
2025-06-26 10:26:00 +0530

    metadata added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -13,3 +14,27 @@ export const formatPrice = (price: number) => {
 
   return formatter.format(price);
 };
+
+export function constructMetadata({
+  title = "CaseCobra - custom high-quality phone cases",
+  description = "Create custom high-quality phone cases in seconds",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    icons,
+    metadataBase: new URL("https://casecobra-tau-six.vercel.app/"),
+  };
+}
